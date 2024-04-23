38a60188411104a978627b52485ba7fa652b3f8d
JingwenQuan <167440718+JingwenQuan@users.noreply.github.com>
2024-04-23 14:31:03 +0800

    Add files via upload

---
+++ build/CMakeFiles/ShortVideoIdentification.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/Code/opensrc/triller_flow/src/fingerprint/smp_fp.cpp" "G:/Code/opensrc/triller_flow/build/CMakeFiles/ShortVideoIdentification.dir/src/fingerprint/smp_fp.cpp.obj"
+  "G:/Code/opensrc/triller_flow/src/triller/H12_flow.cpp" "G:/Code/opensrc/triller_flow/build/CMakeFiles/ShortVideoIdentification.dir/src/triller/H12_flow.cpp.obj"
+  "G:/Code/opensrc/triller_flow/src/triller/main_triller.cpp" "G:/Code/opensrc/triller_flow/build/CMakeFiles/ShortVideoIdentification.dir/src/triller/main_triller.cpp.obj"
+  "G:/Code/opensrc/triller_flow/src/triller/triller_flow.cpp" "G:/Code/opensrc/triller_flow/build/CMakeFiles/ShortVideoIdentification.dir/src/triller/triller_flow.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ShortVideoIdentification.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShortVideoIdentification.dir/src/fingerprint/smp_fp.cpp.obj"
+  "CMakeFiles/ShortVideoIdentification.dir/src/triller/H12_flow.cpp.obj"
+  "CMakeFiles/ShortVideoIdentification.dir/src/triller/main_triller.cpp.obj"
+  "CMakeFiles/ShortVideoIdentification.dir/src/triller/triller_flow.cpp.obj"
+  "ShortVideoIdentification.exe"
+  "ShortVideoIdentification.exe.manifest"
+  "ShortVideoIdentification.pdb"
+  "libShortVideoIdentification.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShortVideoIdentification.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
